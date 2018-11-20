11fc57a7c125fd1e5e0e813719d47348c3a787c5
liuyonghui <1359158019@qq.com>
2018-11-20 18:27:30 +0800

    Finish new_block  hash and last_block

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,34 @@
+import hashlib
+import json
+from time import time
+
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        # creates a new block and adds it to the chain
-        pass
+        # create the genesis block
+        self.new_block(previous_hash = 1,proof = 100)
+
+    def new_block(self, proof, previous_hash = None):
+        """
+        生成新的区块
+        """
+        
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+
+        # reset the current list of transactions
+        self.current_transactions = []
+        
+        self.chain.append(block)
+        return block
     
     def new_transaction(self, sender, recipient, amount):
         """
@@ -27,10 +50,15 @@ class Blockchain(object):
     
     @staticmethod
     def hash(block):
-        # hashes a block
-        pass
-    
+        """
+        生成块的 SHA-256 hash值
+        """
+
+        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode
+        return hashlib.sha256(block_string).hexdigest()
+
     @property
     def last_block(self):
         # return the last block in the chain
-        pass
+        return self.chain[-1]
