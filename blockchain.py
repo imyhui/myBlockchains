541502c329b73807326b53c358b426fbf436dc12
liuyonghui <1359158019@qq.com>
2018-11-20 18:12:36 +0800

    Finish new_transaction

--- blockchain.py
+++ blockchain.py
@@ -7,9 +7,23 @@ class Blockchain(object):
         # creates a new block and adds it to the chain
         pass
     
-    def new_transaction(self):
-        # adds a new transaction to the list of transactions
-        pass
+    def new_transaction(self, sender, recipient, amount):
+        """
+        生成新交易信息，信息将加入到下一个待挖的区块中
+        params:
+            - sender address of sender
+            - recipient address of recipient
+            - amount
+        return:
+            - the index of the Block that will hold this transaction
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+        
+        return self.last_block['index'] + 1
     
     @staticmethod
     def hash(block):
