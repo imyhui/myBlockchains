86105751b7389a83eb2fe3cc24a8107049a525cc
liuyonghui <1359158019@qq.com>
2018-11-20 19:26:27 +0800

    Finish mine api

--- blockchain.py
+++ blockchain.py
@@ -96,7 +96,31 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods = ['GET'])
 def mine():
-    return "We'll mine a new Block"
+    # We run the proof of work algorithm to get the next proof...
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # 给工作量证明的节点提供奖励.
+    # 发送者为 "0" 表明是新挖出的币
+    blockchain.new_transaction(
+        sender = "0",
+        recipient = node_identifier,
+        amount = 1,
+    )
+
+    # Forge the new Block by adding it to the chain
+    block = blockchain.new_block(proof)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash']
+    }
+
+    return jsonify(response), 200
 
 @app.route('/transactions/new', methods = ['POST'])
 def new_transaction():
