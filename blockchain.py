07ae556ea12a59d2fcc74da9eec66f9d8086ed35
liuyonghui <1359158019@qq.com>
2018-11-20 18:50:52 +0800

    Finish proof algorithm

--- blockchain.py
+++ blockchain.py
@@ -62,3 +62,26 @@ class Blockchain(object):
     def last_block(self):
         # return the last block in the chain
         return self.chain[-1]
+
+    def proof_of_work(self, last_proof):
+        """
+        简单的工作量证明:
+         - 查找一个 p' 使得 hash(pp') 以4个0开头
+         - p 是上一个块的证明,  p' 是当前的证明
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
+    
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        验证证明: 是否hash(last_proof, proof)以4个0开头
+        """
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+        
+
